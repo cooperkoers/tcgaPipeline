cca83ea8941d28b513d5d8b640653534702069a2
Cooper Koers <133537769+cooperkoers@users.noreply.github.com>
2024-07-09 14:31:45 -0400

    distances

---
+++ src/distance_calc.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import numpy as np
+
+# different distance calculations for two depth files
+def distance(depth1, depth2, method):
+    # relative distance calculation
+    depth1 = pd.read_csv(depth1, sep="\t", header=None)
+    depth1.columns = ["chrom", "start", "end", "depth"]
+    depth2 = pd.read_csv(depth2, sep="\t", header=None)
+    depth2.columns = ["chrom", "start", "end", "depth"]
+
+    # calculate the relative distance between the two depth files
+    result = []
+    for chrom in depth1["chrom"].unique():
+        for start in depth1[depth1["chrom"] == chrom]["start"].unique():
+            if start in depth2[depth2["chrom"] == chrom]["start"].unique():
+                if method == 'relative':
+                    result.append(
+                        {
+                            "chrom": chrom,
+                            "start": start,
+                            "distance": np.abs(
+                                depth1[
+                                    (depth1["chrom"] == chrom) & (depth1["start"] == start)
+                                ]["depth"]
+                                - depth2[
+                                    (depth2["chrom"] == chrom) & (depth2["start"] == start)
+                                ]["depth"]
+                            ) / (depth1[
+                                (depth1["chrom"] == chrom) & (depth1["start"] == start)
+                                ]["depth"])
+                        }
+                    )
+                if method == 'mse':
+                    result.append(
+                        {
+                            "chrom": chrom,
+                            "start": start,
+                            "distance": np.abs(
+                                depth1[
+                                    (depth1["chrom"] == chrom) & (depth1["start"] == start)
+                                ]["depth"]
+                                - depth2[
+                                    (depth2["chrom"] == chrom) & (depth2["start"] == start)
+                                ]["depth"]
+                            ) ** 2,
+                        }
+                    )
+    # return sum of distances
+    return sum([x["distance"] for x in result])
