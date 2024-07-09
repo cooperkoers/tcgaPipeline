88c5dea7f0abc33c6b4f5d23ab344347679eb5be
Cooper Koers <ck5658@princeton.edu>
2024-07-09 10:38:01 -0400

    edited binning to be compatable with any length and also rkpm

--- src/binning.py
+++ src/binning.py
@@ -10,28 +10,26 @@ def depth_binning(depthFile, bin_size):
     
     The number of counts are then summed for each and this is turned 
     into an rpkm value for each bin.
-
-    example df:
-    chr  start   end     depth
-    chr1 10000   10001   10
-    chr1 10101   10102   20
-    chr1 10202   10203   30
-    chr1 12003   10304   40
     """
     df = pd.read_csv(depthFile, sep='\t', header=None)
-    df.columns = ['chr', 'start', 'end', 'depth']
+    df.columns = ['chrom', 'start', 'end', 'depth']
 
-    # group by chromosome
-    grouped = df.groupby('chr')
-    binned = pd.DataFrame()
+    result = []
 
-    for name, group in grouped:
-        # bin by start and end positions
-        group['bin'] = group['start'] // bin_size
-        group['bin'] = group['bin'] * bin_size
-        group = group.groupby('bin').sum()
-        group['chr'] = name
-        binned = pd.concat([binned, group])
+    for chrom in df['chrom'].unique():
+        chrom_df = df[df['chrom'] == chrom].sort_values(by='start')
+        start_pos = chrom_df['start'].min()
+        end_pos = chrom_df['end'].max()
 
-    # calculate rpkm
+        for region_start in range(start_pos, end_pos, bin_size):
+            region_end = region_start + bin_size
+            region_df = chrom_df[(chrom_df['start'] >= region_start) & (chrom_df['end'] <= region_end)]
+            if not region_df.empty:
+                count_sum = region_df['depth'].sum()
+                row_count = len(region_df)
+                result.append({'chrom': chrom, 'start': region_start, 'end': region_end, 'count_sum': count_sum, 'row_count': row_count}, 'rpkm': count_sum / row_count * 1000000)
     
+    result_df = pd.DataFrame(result)
+    result_df.set_index('chrom', 'start', inplace = True)
+
+    return result_df
